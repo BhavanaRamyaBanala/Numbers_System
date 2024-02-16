959b964d099f9d9e55e5caa2d8e2a216d4a99393
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-16 09:47:02 +0530

    AllFiles

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+Console.WriteLine("Welcome to NumberSystem");
+Console.WriteLine("** Even Numbers **");
+NumberSystem.EvenNum();
+Console.WriteLine("** Odd Numbers **");
+NumberSystem.OddNum();
